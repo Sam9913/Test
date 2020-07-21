a6e94be844d633fa5beecf10632dc997b197cefb
陈诗贤 <sam_tan9913@hotmail.com>
2020-07-21 18:47:17 +0800

    comment more

--- Test/Normal.cpp
+++ Test/Normal.cpp
@@ -1,9 +1,11 @@
 /*This program is an implementaion of the Jacobi iteration method.
-The relation used is
-X(k+1)=(Diagonal matrix Inverse)(RHS Coefficient-)*(Lower triangle+Upper triangle)X(k))
+The relation used is X(k+1)=(Diagonal matrix Inverse)(RHS Coefficient-)*(Lower triangle+Upper triangle)X(k))
 */
-#include<stdio.h>
+#include <stdio.h>
+#include <omp.h>
+#define EPSILON 0.001
 
+char label[3] = { 'x','y','z' };
 float coeff[10][10];
 float Dinv[10][10];
 float approx[10][1];
@@ -13,22 +15,21 @@ float R[10][10];//declare the relevant matrices
 float matrixRes[10][1];
 float b[10][1];
 int row, column, size = 3, navigate;
-float epsilon = (1 + 2 ^ -52) - 1;
 void multiply(float matrixA[][10], float matrixB[][1])
 {
-	int ctr, ictr;
+	int ctr;
 	//function to perform multiplication
 	for (ctr = 0; ctr < size; ctr++)
 	{
+		//before every iteration start it will redefine the matrixRes to 0
 		matrixRes[ctr][0] = 0;
 		for (navigate = 0; navigate < size; navigate++)
 			matrixRes[ctr][0] = matrixRes[ctr][0] + matrixA[ctr][navigate] * matrixB[navigate][0];
 	}
 }
+
 int main()
 {
-	/*printf("Enter the number of unknown(below 10)\n");
-	scanf_s("%d", &size);//enter the size*/
 	printf("Enter the coefficent matrix\n");
 	for (row = 0; row < size; row++)
 		for (column = 0; column < size; column++)
@@ -39,7 +40,12 @@ int main()
 	printf("Enter the RHS coefficient\n");
 	for (row = 0; row < size; row++)
 		scanf_s("%f", &b[row][0]);
-	for (row = 0; row < size; row++)//We calculate the diagonal inverse matrix make all other entries as zero except Diagonal entries whose resciprocal we store
+
+	double start_time = omp_get_wtime();
+
+	//calculating the diagonal inverse matrix make all other entries as zero except Diagonal entries 
+	//whose resciprocal stored
+	for (row = 0; row < size; row++)
 		for (column = 0; column < size; column++)
 		{
 			if (row == column)
@@ -47,8 +53,10 @@ int main()
 			else
 				Dinv[row][column] = 0;
 		}
+
+	//calculating the R matrix L+U
 	for (row = 0; row < size; row++)
-		for (column = 0; column < size; column++)//calculating the R matrix L+U
+		for (column = 0; column < size; column++)
 		{
 			if (row == column)
 				R[row][column] = 0;
@@ -56,38 +64,55 @@ int main()
 				if (row != column)
 					R[row][column] = coeff[row][column];
 		}
-	int iter;
-	//printf("Enter the number of iterations:\n");
-	//scanf_s("%d", &iter);//enter the number of iterations
+
 	int ctr = 1;
 	int octr;
 	int sameAmount = 0;
+
+	//to check whether the current iteration same as the last iteration
 	while (sameAmount != 6)
 	{
 		for (octr = 0; octr < size; octr++) {
-			if (tempApprox[octr][0] - approx[octr][0] < 0.001 && ctr != 1) {
+			if (tempApprox[octr][0] - approx[octr][0] < EPSILON && ctr != 1) {
 				sameAmount++;
 			}
 			tempApprox[octr][0] = matrixRes[octr][0];
 		}
 
+		//If result that the approximation answer of current iteration and last iteration
+		//is the same, then break the while loop
 		if (sameAmount == 6) {
 			break;
 		}
+		//If only two of the answers are same, then reset it to 0
 		else if (sameAmount != 3) {
 			sameAmount = 0;
 		}
 
-		multiply(R, approx);//multiply L+U and the approximation
+		//multiply L+U and the approximation
+		multiply(R, approx);
+
+		//calculate the matrix(b-Rx) and store it to temp
 		for (row = 0; row < size; row++)
-			temp[row][0] = b[row][0] - matrixRes[row][0];//the matrix(b-Rx)
-		multiply(Dinv, temp);//multiply D inverse and (b-Rx)
+			temp[row][0] = b[row][0] - matrixRes[row][0];
+
+		//multiply D inverse and (b-Rx)
+		multiply(Dinv, temp);
+
+		//store matrixRes value in the next approximation
 		for (octr = 0; octr < size; octr++)
-			approx[octr][0] = matrixRes[octr][0];//store matrixRes value in the next approximation
-		printf("The Value after iteration %d is\n", ctr);
-		for (row = 0; row < size; row++){
-			printf("%.3f\n", approx[row][0]);//display the value after the pass
+			approx[octr][0] = matrixRes[octr][0];
+
+		//display the value after the pass
+		printf("The value after iteration %d is\n", ctr);
+		for (row = 0; row < size; row++) {
+			printf("%c : %.3f\n", label[row], approx[row][0]);
 		}
 		ctr++;
-	} 
+	}
+
+	double end_time = omp_get_wtime();
+	//print time used
+	printf("Time used (seconds) : %f", end_time - start_time);
+
 }
